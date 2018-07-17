ba7ad8aadb71a32527e7e8d8a15dfef87ad79f20
Sid Bai <baishisid+github@gmail.com>
2018-07-17 08:16:32 +0000

    add invalid input test case for credential scope

--- aws_sigv4/tests/aws_sigv4_test.c
+++ aws_sigv4/tests/aws_sigv4_test.c
@@ -11,13 +11,31 @@ END_TEST
 
 START_TEST (AwsSigv4Test_CredentialScope)
 {
-    /* happy case */
-    aws_sigv4_params_t sigv4_params;
-    sigv4_params.x_amz_date = "20180717T074800Z";
-    sigv4_params.region     = "us-east-1";
-    sigv4_params.service    = "s3";
     char credential_scope[1024] = { 0 };
-    int len = get_credential_scope(&sigv4_params, credential_scope);
+    aws_sigv4_params_t sigv4_params = { .x_amz_date = NULL,
+                                        .region = NULL,
+                                        .service = NULL };
+    /* invalid input */
+    int len = get_credential_scope(NULL, credential_scope);
+    ck_assert_int_eq(len, 0);
+
+    len = get_credential_scope(&sigv4_params, credential_scope);
+    ck_assert_int_eq(len, 0);
+
+    sigv4_params.x_amz_date = "20180717T074800Z";
+    len = get_credential_scope(&sigv4_params, credential_scope);
+    ck_assert_int_eq(len, 0);
+
+    sigv4_params.region = "us-east-1";
+    len = get_credential_scope(&sigv4_params, credential_scope);
+    ck_assert_int_eq(len, 0);
+
+    sigv4_params.service = "s3";
+    len = get_credential_scope(&sigv4_params, NULL);
+    ck_assert_int_eq(len, 0);
+
+    /* happy case */
+    len = get_credential_scope(&sigv4_params, credential_scope);
     const char* expected_credential_scope = "20180717/us-east-1/s3/aws4_request";
     int expected_len = strlen(expected_credential_scope);
     ck_assert_mem_eq(credential_scope, expected_credential_scope, expected_len);
